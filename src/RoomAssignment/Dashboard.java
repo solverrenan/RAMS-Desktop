a049ba0172268bb0d1b4662ae9ba97ff351805d9
yhammy21 <121219748+yhammy21@users.noreply.github.com>
2023-01-09 13:51:58 +0800

    Update Dashboard.java
    - Changed UI code to resolve merge errors

--- src/RoomAssignment/Dashboard.java
+++ src/RoomAssignment/Dashboard.java
@@ -859,6 +859,7 @@ public class Dashboard extends javax.swing.JFrame {
                             .addComponent(cbRAMUpdateTeacher)
                             .addComponent(cbRAMUpdateTime))
                         .addGap(0, 125, Short.MAX_VALUE)))
+            .addContainerGap()))
         );
         pnlRAMScheduleLayout.setVerticalGroup(
             pnlRAMScheduleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
